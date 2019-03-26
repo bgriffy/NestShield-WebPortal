019469947659a272c2b75d8bf6af7be944cc1294
Brent Griffin <bjg16c@my.fsu.edu>
2019-03-26 12:41:43 -0400

    Having problems with async function for retrieving data. Still working on it.

--- views/app.js
+++ views/app.js
@@ -19,8 +19,11 @@ const userPass = document.getElementById("user-password");
 const loginBtn = document.getElementById("login-btn");
 const signUpBtn = document.getElementById("signup-btn");
 
+//arrays for storage
+let deviceArr = [];
+
 //automatically sign user out for testing purposes
-firebase.auth().signOut();
+// firebase.auth().signOut();
 
 //Add event for login button
 loginBtn.addEventListener("click", function(){
@@ -40,30 +43,42 @@ signUpBtn.addEventListener("click", function(){
   promise.catch(e => console.log(e.message));
 });
 
+
 //add a realtime authentication listener
 firebase.auth().onAuthStateChanged(fireBaseUser => {
   if(fireBaseUser)
   {
     console.log(fireBaseUser);
     //window.location = "/index";
-    alert("You are logged in.")
-    alert("UID: " + fireBaseUser.uid);
+    console.log("You are logged in.")
+    console.log("UID: " + fireBaseUser.uid);
     let uid = fireBaseUser.uid; 
     //get a reference to the database
     let database = firebase.database();
-    let ref = database.ref("users/" + uid).child("devices"); 
-    ref.on("value", gotData, errData);
+    let ref = database.ref("users/").child(uid).child("devices"); 
+    let data = await ref.on("value");
+    currentDevice = Object.keys(data.val())[0];
+    console.log("current device: " + currentDevice);
+    
+
+    // let deviceRef = database.ref("devices/").child(retrievedValue);
+    // deviceRef.on("value", getData2);
     
   }
   else
-    alert("You are not logged in.");
+    console.log("You are not logged in.");
 });
 
 
-function gotData(data)
+function getData(data)
 { 
-  let currentDevice = Object.keys(data.val())[0];
-  return(currentDevice);
+  currentDevice = Object.keys(data.val())[0];
+  console.log("current device: " + currentDevice); 
+}
+
+function gotData2(data)
+{ 
+  console.log("current device: " + data.val());
 }
 
 function errData(error)
@@ -71,4 +86,3 @@ function errData(error)
   console.log("Error!");
   console.log(error);
 }
-
