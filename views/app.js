8f3914ac2960c2656376bb2787325354018f103b
Brent Griffin <bjg16c@my.fsu.edu>
2019-03-26 15:46:42 -0400

    Fixed issue with device query. Am now able to successfully query whitelist entries. Now I just need to get it into HTML.

--- views/app.js
+++ views/app.js
@@ -19,9 +19,6 @@ const userPass = document.getElementById("user-password");
 const loginBtn = document.getElementById("login-btn");
 const signUpBtn = document.getElementById("signup-btn");
 
-//arrays for storage
-let deviceArr = [];
-
 //automatically sign user out for testing purposes
 // firebase.auth().signOut();
 
@@ -42,47 +39,66 @@ signUpBtn.addEventListener("click", function(){
   let promise = firebase.auth().createUserWithEmailAndPassword(uEmail, uPassword);
   promise.catch(e => console.log(e.message));
 });
-
-
+let whiteList = [];
 //add a realtime authentication listener
 firebase.auth().onAuthStateChanged(fireBaseUser => {
   if(fireBaseUser)
   {
     console.log(fireBaseUser);
     //window.location = "/index";
-    console.log("You are logged in.")
-    console.log("UID: " + fireBaseUser.uid);
+    // alert("You are logged in.")
+    // alert("UID: " + fireBaseUser.uid);
+    
     let uid = fireBaseUser.uid; 
     //get a reference to the database
     let database = firebase.database();
-    let ref = database.ref("users/").child(uid).child("devices"); 
-    let data = await ref.on("value");
-    currentDevice = Object.keys(data.val())[0];
-    console.log("current device: " + currentDevice);
-    
-
-    // let deviceRef = database.ref("devices/").child(retrievedValue);
-    // deviceRef.on("value", getData2);
-    
+    //get a data snapshot of the users devices
+    let ref = database.ref("users").child(uid).child("devices"); 
+    /* 
+      This block of code includes a nested query. First we retrieve
+      the device ID, then we use that device ID inside the nested
+      query which retrives the whitelist entry corresponding to the
+      specific device. We repeat this process for every device the
+      user has.  
+    */
+    ref.once("value")
+    //query device list
+    .then(function(data){
+      currentDevice = Object.keys(data.val())[0];
+      console.log("current device: " + currentDevice); 
+      let deviceRef = database.ref("devices/").child(currentDevice).child("whitelist_entries");
+      deviceRef.once("value")
+      //nested query for whitelist entries  
+      .then(function(snap){
+        snap.forEach(function(childSnapshot) {
+          var entryName = childSnapshot.val();
+          whiteList.push(entryName); 
+        });
+        //print array for testing purposes
+        whiteList.forEach(function(entryName) {
+          console.log(entryName);
+        });
+      })   
+    })
   }
   else
     console.log("You are not logged in.");
 });
 
 
-function getData(data)
+
+
+function getData(data, callback)
 { 
   currentDevice = Object.keys(data.val())[0];
   console.log("current device: " + currentDevice); 
+  return (callback);
 }
 
-function gotData2(data)
-{ 
-  console.log("current device: " + data.val());
-}
 
 function errData(error)
 {
   console.log("Error!");
   console.log(error);
 }
+
