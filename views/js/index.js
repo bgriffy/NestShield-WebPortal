a01d70f33465928f38176340f82c528aa60b8b3f
Brent Griffin <bjg16c@my.fsu.edu>
2019-04-23 23:18:42 -0400

    Am now able to view all devices on Dashboard page--both android and windows. Going to refactor.

--- views/js/index.js
+++ views/js/index.js
@@ -72,31 +72,92 @@ firebase.auth().onAuthStateChanged(fireBaseUser => {
                         let childProfile = {
                             profileName: snapshot.child("child_name").val(),
                             whiteList: Object.values((snapshot.child("whitelist_entries").val())),
-                            deviceName: snapshot.child("child_name").val() + "'s Android device"
+                            deviceName: snapshot.child("child_name").val() + "-Android"
                         };
-
-                        console.log("childProfile: " + childProfile);
                         
                         childProfiles.push(childProfile);
 
                         //store childs name into array
                         if(!childrenNames.includes(childProfile.profileName))
                             childrenNames.push(childProfile.profileName);
+                        
+                        //send child-name tab headers to HTML
+                    childrenNames.forEach(function(childName){
+                        let childDiv = childName + "Div";
+                        let childElem = document.getElementById(childDiv);
+                        //check to make sure childs name not listed already
+                        if(childElem == null)
+                        {
+                            printHead = "<div class ='childHeader' id = '" + childDiv + "'</div>";
+                            document.getElementById('childTabs').innerHTML += printHead;
+                        }
+                        printHead = "<ul class='nav nav-tabs'>"
+                            + "<li class = 'dropdown'>"
+                            + "<a data-toggle='dropdown' class='dropdown-toggle' href='#'>"
+                            + childName + "</a>"
+                            + "<ul class='dropdown-menu'>"
+                            childProfiles.forEach(function(childProfile){
+                                if(childProfile.profileName == childName)
+                                {
+                                    printHead += "<li><a class = 'dropLink' data-toggle='tab' href='#" 
+                                    + childProfile.deviceName + "-"
+                                    + childProfile.profileName + "'>"
+                                    + childProfile.deviceName
+                                    + "</a></li>";
+                                }
+                            });
+                            printHead += "</ul></li></ul>";
+                            document.getElementById(childDiv).innerHTML = printHead;
+                    });
+
+                    let tabClass = "' class='contentTab tab-pane fade'>";
+
+                    //send child profiles to HTML
+                    childProfiles.forEach(function(childProfile){
+                        let whiteListDiv = childProfile.deviceName + "-" + childProfile.profileName;
+                        let whiteListElem = document.getElementById(whiteListDiv);
+                        if(whiteListElem == null)
+                        {
+                            printEntries += "<div id='" + whiteListDiv + tabClass
+                            + "<table class='table table-striped'>"
+                            + "<thead class='thead'>"
+                            + "<tr>"
+                            + "<th scope='col'>Process</th>"
+                            + "<th scope='col'></th>"
+                            + "<th scope='col'>Options</th>"
+                            + "<th scope='col'></th>"
+                            + "</tr>"
+                            + "</thead>"
+                            + "<tbody>";
+                            
+                            tabClass = "' class='contentTab tab-pane fade'>";
+                            childProfile.whiteList.forEach(function(entryName){
+                                
+                                printEntries += "<tr>" 
+                                + "<td>"+entryName+"</td>"
+                                + "<td><button type='button' class='btn btn-secondary btn-sm'>Monitor</button></td>"
+                                + "<td><button type='button' class='btn btn-secondary btn-sm'>Restrict</button></td>"
+                                + "<td><button type='button' class='btn btn-secondary btn-sm'>Delete</button></td>"
+                                + "</tr>";
+                            });
+        
+                            printEntries += "</tbody>"
+                            +"</table>"
+                            +"</div>"
+                        }
+                });
+                document.getElementById('tab-content').innerHTML = printEntries;
                       }
                     else
                       androidDevice = false; 
                 });
 
-
-                //get a data snapshot of the accounts associated with Windows devices 
-                if(androidDevice == false)
-                    windowsRef = database.ref("devices/").child(currentDevice).child("accounts"); 
-                
+                //----windows devices----
+                windowsRef = database.ref("devices/").child(currentDevice).child("accounts"); 
                 windowsRef.once("value").then(function(snapshot){
                     
                     if (snapshot.exists())
                     {   
-                        console.log("current device from snap:" + currentDevice);
                         //get list of the accounts
                         accounts = Object.keys(snapshot.val());
                         
@@ -148,7 +209,7 @@ firebase.auth().onAuthStateChanged(fireBaseUser => {
                                 }
                             });
                             printHead += "</ul></li></ul>";
-                            document.getElementById(childDiv).innerHTML = printHead;
+                            document.getElementById(childDiv).innerHTML += printHead;
                     });
 
                     let tabClass = "' class='contentTab tab-pane fade show active'>";
@@ -187,7 +248,7 @@ firebase.auth().onAuthStateChanged(fireBaseUser => {
                             +"</div>"
                         }
                 });
-                $(document).find('#tab-content').html(printEntries);
+                document.getElementById('tab-content').innerHTML += printEntries;
                 androidDevice = false;
                 });  
             });
